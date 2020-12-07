1406af5e70a56c9fc125cc67c18950b31dbfc5ab
Zhang, Feng <15110700005@fudan.edu.cn>
2020-12-07 10:56:14 +0800

    Update step1_installPackage.R

--- SUP/step1_installPackage.R
+++ SUP/step1_installPackage.R
@@ -1,4 +1,24 @@
 
+# https://bioconductor.org/packages/release/bioc/html/DESeq2.html
+# Install DESeq2
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("DESeq2")
 
 
+#####################################
+# Install ComplexHeatmap and dependencies...
+install packages('circlize')
+install packages('seriation')
 
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install("ComplexHeatmap")
+######################################
+
+
+######################################
+# Install GSEA
+
+# Please check https://www.gsea-msigdb.org/gsea/index.jsp
